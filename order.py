9ba270d49d531f7cadf1bb4579e9a7e2e502a3af
Grace Zhou <624gracezhou@gmail.com>
2024-11-22 09:32:46 -0600

    Update order.py

--- order.py
+++ order.py
@@ -72,10 +72,11 @@ class Order:
             return random_list[index]
 
         item_descriptions = [item.to_string().capitalize() for item in self.items]
-        items_str = ", ".join(item_descriptions) + "."
+        # items_str = ", ".join(item_descriptions) + "."
+        items_str = "\n".join(item_descriptions)
         total_str = f"Total Price: ${self.total_price:.2f}"
 
-        return f"\n\n{items_str}\n\n{total_str}."
+        return f"\n\n{items_str}\n\n{total_str}." 
 
         """ item_descriptions = [item.to_string() for item in self.items.values()]
         items_str = "\n".join(item_descriptions)
